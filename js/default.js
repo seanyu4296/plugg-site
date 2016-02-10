cfb194a964e4dbc4d43082c7c016a8bccc472982
Sean Yu <seanyu4296@gmail.com>
2016-02-10 21:57:52 +0800

    initial commit

---
+++ js/default.js
@@ -0,0 +1,18 @@
+/**
+ * Created by Sean Yu on 6 Feb 2016.
+ */
+
+$(".animsition").animsition({
+    inClass: 'fade-in',
+    outClass: 'fade-out',
+    linkElement: 'header a',
+    inDuration: 1000,
+    outDuration: 500
+});
+
+/*$('.navbar').sticky({
+    getWidthFrom: '.container',
+    responsiveWidth: true
+});*/
+
+$
